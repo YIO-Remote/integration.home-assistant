73f2d3e12a2e3a81276dd21c0d47b9f7bf0d2afe
Christian Riedl <christian.markus.riedl@outlook.com>
2019-12-10 15:45:01 +0100

    Update interface and enums

--- homeassistant.h
+++ homeassistant.h
@@ -1,26 +1,3 @@
-/******************************************************************************
- *
- * Copyright (C) 2019 Marton Borzak <hello@martonborzak.com>
- * Copyright (C) 2019 Christian Riedl <ric@rts.co.at>
- *
- * This file is part of the YIO-Remote software project.
- *
- * YIO-Remote software is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * YIO-Remote software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with YIO-Remote software. If not, see <https://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
- *****************************************************************************/
-
 #ifndef HOMEASSISTANT_H
 #define HOMEASSISTANT_H
 
@@ -31,9 +8,10 @@
 #include <QtWebSockets/QWebSocket>
 #include <QTimer>
 #include <QThread>
+#include <QLoggingCategory>
 
 #include "../remote-software/sources/integrations/integration.h"
-#include "../remote-software/sources/integrations/integrationinterface.h"
+#include "../remote-software/sources/integrations/plugininterface.h"
 #include "../remote-software/sources/entities/entitiesinterface.h"
 #include "../remote-software/sources/entities/entityinterface.h"
 #include "../remote-software/sources/notificationsinterface.h"
@@ -43,16 +21,24 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// HOME ASSISTANT FACTORY
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class HomeAssistant : public IntegrationInterface
+class HomeAssistantPlugin : public PluginInterface
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "YIO.IntegrationInterface" FILE "homeassistant.json")
-    Q_INTERFACES(IntegrationInterface)
+    Q_PLUGIN_METADATA(IID "YIO.PluginInterface" FILE "homeassistant.json")
+    Q_INTERFACES(PluginInterface)
 
 public:
-    explicit HomeAssistant() {}
+    explicit HomeAssistantPlugin() :
+        m_log("homeassistant")
+    {}
 
     void create                     (const QVariantMap& config, QObject *entities, QObject *notifications, QObject* api, QObject *configObj) override;
+    void setLogEnabled              (QtMsgType msgType, bool enable) override
+    {
+        m_log.setEnabled(msgType, enable);
+    }
+private:
+    QLoggingCategory    m_log;
 };
 
 
@@ -65,27 +51,26 @@ class HomeAssistantBase : public Integration
     Q_OBJECT
 
 public:
-    explicit HomeAssistantBase(QObject *parent);
+    explicit HomeAssistantBase(QLoggingCategory& log, QObject *parent);
     virtual ~HomeAssistantBase();
 
     Q_INVOKABLE void setup  	    (const QVariantMap& config, QObject *entities, QObject *notifications, QObject* api, QObject *configObj);
     Q_INVOKABLE void connect	    ();
     Q_INVOKABLE void disconnect	    ();
+    Q_INVOKABLE void sendCommand    (const QString& type, const QString& entity_id, int command, const QVariant& param);
 
 signals:
     void connectSignal              ();
     void disconnectSignal           ();
-    void sendCommandSignal          (const QString& type, const QString& entity_id, const QString& command, const QVariant& param);
+    void sendCommandSignal          (const QString& type, const QString& entity_id, int command, const QVariant& param);
 
 
 public slots:
-    void sendCommand                (const QString& type, const QString& entity_id, const QString& command, const QVariant& param);
     void stateHandler               (int state);
 
 private:
-    void updateEntity               (const QString& entity_id, const QVariantMap& attr) {}
-
     QThread                         m_thread;
+    QLoggingCategory&               m_log;
 };
 
 
@@ -98,7 +83,8 @@ class HomeAssistantThread : public QObject
     Q_OBJECT
 
 public:
-    HomeAssistantThread             (const QVariantMap &config, QObject *entities, QObject *notifications, QObject *api, QObject *configObj);
+    HomeAssistantThread             (const QVariantMap &config, QObject *entities, QObject *notifications, QObject *api, QObject *configObj,
+                                     QLoggingCategory& log);
 
 signals:
     void stateChanged               (int state);
@@ -107,7 +93,7 @@ public slots:
     void connect                    ();
     void disconnect                 ();
 
-    void sendCommand                (const QString& type, const QString& entity_id, const QString& command, const QVariant& param);
+    void sendCommand                (const QString& type, const QString& entity_id, int command, const QVariant& param);
 
     void onTextMessageReceived	    (const QString &message);
     void onStateChanged             (QAbstractSocket::SocketState state);
@@ -141,7 +127,7 @@ private:
     int                             m_tries;
     int                             m_webSocketId;
     bool                            m_userDisconnect = false;
-
+    QLoggingCategory&               m_log;
     int                             m_state = 0;
 
 };
