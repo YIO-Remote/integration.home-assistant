2ac48132d24ba37216bead83dcddc84cfea6047d
Workflow <hello@yio-remote.com>
2019-12-31 00:07:33 +0000

    Update file "/integration.home-assistant/translated/*.ts" from "YIO-Remote/translations"

---
+++ translations/nl_NL.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl" sourcelanguage="en">
+  <context>
+    <name>HomeAssistantThread</name>
+    <message>
+      <location filename="../homeassistant.cpp" line="257"/>
+      <source>Cannot connect to Home Assistant.</source>
+      <translation>Verbinding maken met Home Assistant is mislukt.</translation>
+    </message>
+    <message>
+      <location filename="../homeassistant.cpp" line="257"/>
+      <source>Reconnect</source>
+      <translation>Opnieuw verbinden</translation>
+    </message>
+  </context>
+</TS>
---
+++ translations/zh_TW.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh-TW" sourcelanguage="en">
+  <context>
+    <name>HomeAssistantThread</name>
+    <message>
+      <location filename="../homeassistant.cpp" line="257"/>
+      <source>Cannot connect to Home Assistant.</source>
+      <translation>無法連接到Home Assistant</translation>
+    </message>
+    <message>
+      <location filename="../homeassistant.cpp" line="257"/>
+      <source>Reconnect</source>
+      <translation>重新連線</translation>
+    </message>
+  </context>
+</TS>
