04319ccfa1510faa8d3d10064875c1d21eba24b2
Markus Zehnder <business@markuszehnder.ch>
2020-01-17 20:32:38 +0100

    refactor: Integration interface refactoring
    - Using the base integration interfaces instead of generic QObject.
    
    Part of YIO-Remote/remote-software#348

--- src/homeassistant.h
+++ src/homeassistant.h
@@ -39,84 +39,46 @@
 #include "yio-interface/plugininterface.h"
 #include "yio-interface/yioapiinterface.h"
 #include "yio-plugin/integration.h"
+#include "yio-plugin/plugin.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// HOME ASSISTANT FACTORY
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class HomeAssistantPlugin : public PluginInterface {
+class HomeAssistantPlugin : public Plugin {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "YIO.PluginInterface" FILE "homeassistant.json")
     Q_INTERFACES(PluginInterface)
+    Q_PLUGIN_METADATA(IID "YIO.PluginInterface" FILE "homeassistant.json")
 
  public:
-    HomeAssistantPlugin() : m_log("homeassistant") {}
+    HomeAssistantPlugin();
 
-    void create(const QVariantMap& config, QObject* entities, QObject* notifications, QObject* api,
-                QObject* configObj) override;
-    void setLogEnabled(QtMsgType msgType, bool enable) override { m_log.setEnabled(msgType, enable); }
-
- private:
-    QLoggingCategory m_log;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//// HOME ASSISTANT BASE CLASS
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class HomeAssistantBase : public Integration {
-    Q_OBJECT
-
- public:
-    explicit HomeAssistantBase(QLoggingCategory& log, QObject* parent);  // NOLINT can't use const
-    ~HomeAssistantBase() override;
-
-    Q_INVOKABLE void setup(const QVariantMap& config, QObject* entities, QObject* notifications, QObject* api,
-                           QObject* configObj);
-    Q_INVOKABLE void connect() override;
-    Q_INVOKABLE void disconnect() override;
-    Q_INVOKABLE void sendCommand(const QString& type, const QString& entity_id, int command,
-                                 const QVariant& param) override;
-
- signals:
-    void connectSignal();
-    void disconnectSignal();
-    void sendCommandSignal(const QString& type, const QString& entity_id, int command, const QVariant& param);
-
- public slots:  // NOLINT open issue: https://github.com/cpplint/cpplint/pull/99
-    // FIXME use enum
-    void stateHandler(int state);
-
- private:
-    QThread           m_thread;
-    QLoggingCategory& m_log;
+    // Plugin interface
+ protected:
+    Integration* createIntegration(const QVariantMap& config, EntitiesInterface* entities,
+                                   NotificationsInterface* notifications, YioAPIInterface* api,
+                                   ConfigInterface* configObj) override;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// HOME ASSISTANT THREAD CLASS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class HomeAssistantThread : public QObject {
+class HomeAssistant : public Integration {
     Q_OBJECT
 
  public:
-    HomeAssistantThread(const QVariantMap& config, QObject* entities, QObject* notifications, QObject* api,
-                        QObject* configObj, Integration* baseObj,
-                        QLoggingCategory& log);  // NOLINT can't use const
+    HomeAssistant(const QVariantMap& config, EntitiesInterface* entities, NotificationsInterface* notifications,
+                  YioAPIInterface* api, ConfigInterface* configObj, Plugin* plugin);
 
- signals:
-    // FIXME use enum
-    void stateChanged(int state);
+    Q_INVOKABLE void connect() override;
+    Q_INVOKABLE void disconnect() override;
+    Q_INVOKABLE void sendCommand(const QString& type, const QString& entity_id, int command,
+                                 const QVariant& param) override;
 
  public slots:  // NOLINT open issue: https://github.com/cpplint/cpplint/pull/99
-    void connect();
-    void disconnect();
-
-    void sendCommand(const QString& type, const QString& entity_id, int command, const QVariant& param);
-
     void onTextMessageReceived(const QString& message);
     void onStateChanged(QAbstractSocket::SocketState state);
     void onError(QAbstractSocket::SocketError error);
-
     void onTimeout();
 
  private:
@@ -130,24 +92,12 @@ class HomeAssistantThread : public QObject {
     void updateMediaPlayer(EntityInterface* entity, const QVariantMap& attr);
     void updateClimate(EntityInterface* entity, const QVariantMap& attr);
 
-    // FIXME use enum
-    void setState(int state);
-
-    EntitiesInterface*      m_entities;
-    NotificationsInterface* m_notifications;
-    YioAPIInterface*        m_api;
-    ConfigInterface*        m_config;
-    Integration*            m_baseObj;
-
-    QString m_id;
-
-    QString           m_ip;
-    QString           m_token;
-    QWebSocket*       m_webSocket;
-    QTimer*           m_wsReconnectTimer;
-    int               m_tries;
-    int               m_webSocketId;
-    bool              m_userDisconnect = false;
-    QLoggingCategory& m_log;
-    int               m_state = 0;
+ private:
+    QString     m_ip;
+    QString     m_token;
+    QWebSocket* m_webSocket;
+    QTimer*     m_wsReconnectTimer;
+    int         m_tries;
+    int         m_webSocketId;
+    bool        m_userDisconnect = false;
 };
