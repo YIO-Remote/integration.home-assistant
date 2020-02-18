56c3129fc31d70e7589f7c057e1aefe34bd9644f
Marton <hello@martonborzak.com>
2020-02-18 21:32:25 +0100

    Update en_US.ts

--- translations/en_US.ts
+++ translations/en_US.ts
@@ -2,15 +2,21 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>HomeAssistantThread</name>
+    <name>HomeAssistant</name>
     <message>
-        <location filename="../src/homeassistant.cpp" line="248"/>
+        <location filename="../src/homeassistant.cpp" line="194"/>
         <source>Cannot connect to Home Assistant.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/homeassistant.cpp" line="248"/>
+        <location filename="../src/homeassistant.cpp" line="194"/>
+        <location filename="../src/homeassistant.cpp" line="532"/>
         <source>Reconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/homeassistant.cpp" line="532"/>
+        <source>Connection lost to </source>
         <translation type="unfinished"></translation>
     </message>
 </context>
