bc354f0f99aec24cf9e4742ce8d87e013d0168fe
Workflow <hello@yio-remote.com>
2020-04-15 19:55:46 +0000

    Update file "/integration.home-assistant/translated/*.ts" from "YIO-Remote/translations"

--- translations/fr_FR.ts
+++ translations/fr_FR.ts
@@ -16,7 +16,7 @@
     <message>
       <location filename="../src/homeassistant.cpp" line="556"/>
       <source>Connection lost to </source>
-      <translation type="unfinished">Connection lost to </translation>
+      <translation>Connexion perdue avec </translation>
     </message>
   </context>
 </TS>
