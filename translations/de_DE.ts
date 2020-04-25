871aba0df9ad13e09302d353b6c19d02ee9f75a2
Workflow <hello@yio-remote.com>
2020-04-25 14:46:40 +0000

    Update file "/integration.home-assistant/translated/*.ts" from "YIO-Remote/translations"

--- translations/de_DE.ts
+++ translations/de_DE.ts
@@ -6,17 +6,17 @@
     <message>
       <location filename="../src/homeassistant.cpp" line="205"/>
       <source>Cannot connect to Home Assistant.</source>
-      <translation type="unfinished">Cannot connect to Home Assistant.</translation>
+      <translation>Verbindung zu Home Assistant fehlgeschlagen.</translation>
     </message>
     <message>
       <location filename="../src/homeassistant.cpp" line="556"/>
       <source>Connection lost to </source>
-      <translation type="unfinished">Connection lost to </translation>
+      <translation>Verbindung verloren zu </translation>
     </message>
     <message>
       <location filename="../src/homeassistant.cpp" line="556"/>
       <source>Connection lost to </source>
-      <translation type="unfinished">Connection lost to </translation>
+      <translation>Verbindung verloren zu </translation>
     </message>
   </context>
 </TS>
