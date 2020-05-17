c4d377e0ab5a499308f8029ac054905c04d2efcc
Workflow <hello@yio-remote.com>
2020-05-17 17:45:46 +0000

    ci: New translations

--- translations/ca_ES.ts
+++ translations/ca_ES.ts
@@ -6,18 +6,18 @@
     <message>
       <location filename="../src/homeassistant.cpp" line="221"/>
       <source>Cannot connect to </source>
-      <translation type="unfinished">Cannot connect to </translation>
+      <translation>No es pot connectar amb </translation>
     </message>
     <message>
       <location filename="../src/homeassistant.cpp" line="221"/>
       <location filename="../src/homeassistant.cpp" line="589"/>
       <source>Reconnect</source>
-      <translation type="unfinished">Reconnect</translation>
+      <translation>Reconnectar</translation>
     </message>
     <message>
       <location filename="../src/homeassistant.cpp" line="251"/>
       <source>SSL certificate validation error. Please check your certificate.</source>
-      <translation type="unfinished">SSL certificate validation error. Please check your certificate.</translation>
+      <translation>Error de validació del certificat SSL. Si us plau, revisa el certificat.</translation>
     </message>
     <message>
       <location filename="../src/homeassistant.cpp" line="589"/>
