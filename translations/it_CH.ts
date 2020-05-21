638a9552aeaad4127fb0484097236d7181eb5c5a
Workflow <hello@yio-remote.com>
2020-05-21 16:47:27 +0000

    ci: New translations

---
+++ translations/it_CH.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it-CH" sourcelanguage="en">
+  <context>
+    <name>HomeAssistant</name>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="221"/>
+      <source>Cannot connect to </source>
+      <translation type="unfinished">Cannot connect to </translation>
+    </message>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="221"/>
+      <location filename="../src/homeassistant.cpp" line="589"/>
+      <source>Reconnect</source>
+      <translation type="unfinished">Reconnect</translation>
+    </message>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="251"/>
+      <source>SSL certificate validation error. Please check your certificate.</source>
+      <translation type="unfinished">SSL certificate validation error. Please check your certificate.</translation>
+    </message>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="589"/>
+      <source>Connection lost to </source>
+      <translation type="unfinished">Connection lost to </translation>
+    </message>
+  </context>
+</TS>
