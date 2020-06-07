889c062ff15fbca50c4baa43cf37ba06052221b6
Marton <hello@martonborzak.com>
2020-06-07 15:19:27 +0200

    ci: update dependency

--- translations/en_US.ts
+++ translations/en_US.ts
@@ -4,23 +4,23 @@
 <context>
     <name>HomeAssistant</name>
     <message>
-        <location filename="../src/homeassistant.cpp" line="221"/>
+        <location filename="../src/homeassistant.cpp" line="237"/>
         <source>Cannot connect to </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/homeassistant.cpp" line="221"/>
-        <location filename="../src/homeassistant.cpp" line="589"/>
+        <location filename="../src/homeassistant.cpp" line="237"/>
+        <location filename="../src/homeassistant.cpp" line="632"/>
         <source>Reconnect</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/homeassistant.cpp" line="251"/>
-        <source>SSL certificate validation error. Please check your certificate.</source>
+        <location filename="../src/homeassistant.cpp" line="267"/>
+        <source>SSL certificate validation error. Please check your certificate. </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/homeassistant.cpp" line="589"/>
+        <location filename="../src/homeassistant.cpp" line="632"/>
         <source>Connection lost to </source>
         <translation type="unfinished"></translation>
     </message>
